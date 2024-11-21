a85a34fc2282c2725da7f6df9814d4b74e59e84e
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-11-21 13:10:16 +0530

    Update help.py

--- YTMUSIC/plugins/bot/help.py
+++ YTMUSIC/plugins/bot/help.py
@@ -118,7 +118,7 @@ async def mb_plugin_button(client, CallbackQuery):
     keyboard = InlineKeyboardMarkup([
     [InlineKeyboardButton("⏤͟͟͞͞★ ᴜᴛᴛᴀᴍ", url="https://t.me/UTTAM470"),  # Back Button
      InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/BABY09_WORLD")],  # Update Button
-    [InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data="modebot_cb")]  # Support Button
+    [InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data="ubot_cb")]  # Support Button
 ])
     if cb == "Okieeeeee":
         await CallbackQuery.edit_message_text(f"`something errors`",reply_markup=keyboard,parse_mode=enums.ParseMode.MARKDOWN)
